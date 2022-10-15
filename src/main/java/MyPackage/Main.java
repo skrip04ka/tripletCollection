c4ea667b46997de92ac16fb22155281ad16ad4fc
skrip04ka <fury.2489@gmail.com>
2022-10-16 02:48:39 +0300

    first commit

--- src/main/java/MyPackage/Main.java
+++ src/main/java/MyPackage/Main.java
@@ -8,18 +8,6 @@ public class Main {
 
         TripletCollection<Integer> triplet = new TripletCollection<>();
 
-        for (int i = 0; i < 8; i++) {
-            triplet.addLast(i);
-            triplet.printAll();
-        }
-
-        System.out.println("\n");
-
-        for (int i = 0; i < 8; i++) {
-            triplet.addFirst(i);
-            triplet.printAll();
-        }
-
     }
 
 }

d8a522c8caacaea7fa75e3b888e59e16ad90dc6d
skrip04ka <fury.2489@gmail.com>
2022-10-18 00:25:17 +0300

    V2

--- src/main/java/MyPackage/Main.java
+++ src/main/java/MyPackage/Main.java
@@ -1,13 +1,10 @@
 package MyPackage;
 
-
-import java.util.ArrayDeque;
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
 
         TripletCollection<Integer> triplet = new TripletCollection<>();
+        triplet.addFirst(1);
 
     }
 
